bc0b90a8bc321e9815a54d126cb5ede224400c07
AbedX69 <abedx69@gmail.com>
2024-08-30 08:14:14 +0300

    good error message for signup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/register', async (req, res) => {
     const newUser = await User.createUser(req.body);
     res.status(201).json(newUser);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ message: 'User already exists.' });
   }
 });
 
